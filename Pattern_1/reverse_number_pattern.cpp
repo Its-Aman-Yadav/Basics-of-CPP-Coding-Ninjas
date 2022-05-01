703085cc46d7767b4146e58854c55cdac5cd9bc6
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-05-01 11:27:52 +0530

    Create reverse_number_pattern.cpp

---
+++ Pattern_1/reverse_number_pattern.cpp
@@ -0,0 +1,52 @@
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+21
+321
+4321
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+1
+21
+321
+4321
+54321
+Sample Input 2:
+6
+Sample Output 2:
+1
+21
+321
+4321
+54321
+654321*/
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout << i-j+1;
+            
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+return 0;
+}
