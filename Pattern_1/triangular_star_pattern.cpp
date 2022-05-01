025972fb63f6d3ee460355d8b3371e29760e1583
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-05-01 11:12:31 +0530

    Create triangular_star_pattern.cpp

---
+++ Pattern_1/triangular_star_pattern.cpp
@@ -0,0 +1,60 @@
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+*
+**
+***
+****
+Note : There are no spaces between the stars (*).
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+*
+**
+***
+****
+*****
+Sample Input 2:
+6
+Sample Output 2:
+*
+**
+***
+****
+*****
+*******/
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int i=1;
+    while (i<=n){
+        int j=1;
+        while (j<=i){
+            cout << '*';
+            j++;
+        }
+        cout<<endl;
+        i++;
+        
+    }
+    
+
+return 0;
+}
+
+  
+
+
+
