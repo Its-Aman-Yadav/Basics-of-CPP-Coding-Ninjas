9cac80b72654de9608e6dcb71412b626bb3817d4
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-05-04 23:53:43 +0530

    Create Nth Fabonacci Number.cpp

---
+++ Lecture 6: Operators and For loop/Nth Fabonacci Number.cpp
@@ -0,0 +1,52 @@
+/*Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+    F(n) = F(n-1) + F(n-2), 
+    Where, F(1) = F(2) = 1
+    
+Provided N you have to find out the Nth Fibonacci Number.
+
+Input Format :
+The first line of each test case contains a real number ‘N’.
+
+Output Format :
+For each test case, return its equivalent Fibonacci number.
+
+Constraints:
+1 <= N <= 10000     
+
+Where ‘N’ represents the number for which we have to find its equivalent Fibonacci number.
+
+Time Limit: 1 second
+
+Sample Input 1:
+6
+
+Sample Output 1:
+8
+
+Explanation of Sample Input 1:
+Now the number is ‘6’ so we have to find the “6th” Fibonacci number
+So by using the property of the Fibonacci series i.e 
+
+[ 1, 1, 2, 3, 5, 8]
+So the “6th” element is “8” hence we get the output.*/
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+#include<iostream>
+using namespace std;
+
+int fabo(int n){
+    if (n==1 || n==0){
+        return 1;
+    }
+
+    else{
+        return fabo(n-1) + fabo(n-2);
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    cout << fabo(n-1);
+return 0;
+}
