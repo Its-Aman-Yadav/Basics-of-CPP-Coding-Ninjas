25d33c30788293c2a3b2808d389f44408beb88e4
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-05-08 01:36:08 +0530

    Create Character Pattern.cpp

---
+++ Pattern_1/Character Pattern.cpp
@@ -0,0 +1,59 @@
+/*Print the following pattern for the given N number of rows.
+Pattern for N = 4
+A
+BC
+CDE
+DEFG
+
+Input format :
+Integer N (Total no. of rows)
+
+Output format :
+Pattern in N lines
+
+Constraints
+0 <= N <= 13
+
+Sample Input 1:
+5
+
+Sample Output 1:
+A
+BC
+CDE
+DEFG
+EFGHI
+
+Sample Input 2:
+6
+
+Sample Output 2:
+A
+BC
+CDE
+DEFG
+EFGHI
+FGHIJK*/
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+    int i=1;
+    while (i<=n){
+        int j=1;
+        
+        while(j<=i){
+            int p=+i+63+j;
+            cout <<(char) p;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+return 0;
+}
