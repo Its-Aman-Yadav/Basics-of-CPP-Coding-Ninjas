b45bf02ca23354f1873a02498f680f807f5b8917
Mr Aman yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2022-05-15 00:03:09 +0530

    Create Print All Substrings.cpp

---
+++ Character Array and 2D Array/Print All Substrings.cpp
@@ -0,0 +1,54 @@
+/*For a given input string(str), write a function to print all the possible substrings.
+Substring
+A substring is a contiguous sequence of characters within a string. 
+
+Example: "cod" is a substring of "coding". Whereas, "cdng" is not as the characters taken are not contiguous
+
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+
+Output Format:
+Print the total number of substrings possible, where every substring is printed on a single line and hence the total number of output lines will be equal to the total number of substrings.
+
+Note:
+The order in which the substrings are printed, does not matter.
+
+Constraints:
+0 <= N <= 10^6
+
+Where N is the length of the input string.
+
+Time Limit: 1 second
+
+Sample Input 1:
+abc
+
+Sample Output 1:
+a 
+ab 
+abc 
+b 
+bc 
+c 
+
+ Sample Input 2:
+co
+
+Sample Output 2:
+c 
+co 
+o*/
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+#include<cstring>
+void printSubstrings(char str[]){
+    for(int start = 0; str[start] != '\0'; start++){
+        for(int end = start; str[end] != '\0'; end++){
+            for(int i = start; i <= end; i++){
+                cout << str[i];
+            }
+            cout << endl;
+        }
+    }
+}
